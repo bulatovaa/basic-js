b6381f1b18595b982f3094e7a69caf024b594206
Ekaterina Bulatova <katerinavitalevna@ya.ru>
2020-09-30 11:47:22 +0400

    feat: try to solve some tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw Error;
+  }
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      result.push(arr[i + 1]);
+    } 
+    else if (arr[i] === '--discard-prev') {
+      result.push(arr[i - 1]);
+    }
+    else if (arr[i] === '--double-next') {
+      result.push(arr[i + 1], arr[i + 1]);
+    }
+    else if (arr[i] === '--double-prev') {
+      result.push(arr[i - 1], arr[i - 1]);
+    }
+    else {
+      result.push(arr[i]);
+    }
+  };
+  return result;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date.length === 0) {
+    return 'Unable to determine the time of year!';
+  }
+  if (typeof date !== 'object' && date === null) {
+    throw Error;
+  }
+
+  const month = date.getUTCMonth();
+  if (month >= 0 && month < 2 || month === 11) {
+    return 'winter';
+  }
+  if (month >= 2 && month < 5) {
+    return 'spring';
+  }
+  if (month > 4 && month < 8) {
+    return 'summer';
+  }
+  if (month > 7 && month < 11) {
+    return 'fall';
+  }
 };
