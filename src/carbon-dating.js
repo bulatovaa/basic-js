5cfc8a55a9af6a132cfc912a638dee6c7da5d4b7
Ekaterina Bulatova <katerinavitalevna@ya.ru>
2020-10-19 11:38:49 +0400

    feat: solve carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,8 +4,8 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (typeof sampleActivity !== 'string' || sampleActivity !== 'string') {
-    return false;
+  if (sampleActivity === 'string') {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
   }
-  return Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
+  return false;
 };
