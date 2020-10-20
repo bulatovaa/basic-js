4ffa752c4241fa54827c2c1be80f88cbcd04c9f1
Ekaterina Bulatova <katerinavitalevna@ya.ru>
2020-10-20 12:30:29 +0400

    feat: solve hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,7 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-    const obj = {};
-    
+    let obj = {};
+    obj.turns = 0;
+    obj.seconds = 0;
 
+    obj.turns = Math.pow(2, disksNumber) - 1;
+    obj.seconds = Math.floor(obj.turns / (turnsSpeed / 3600));
+    return obj;
 };
