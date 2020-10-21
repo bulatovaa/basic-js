0f7ad9d62407d112e9b35eca7b6408e3778fd685
Ekaterina Bulatova <katerinavitalevna@ya.ru>
2020-10-21 11:34:48 +0400

    feat: solve transfotm-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -6,21 +6,26 @@ module.exports = function transform(arr) {
   }
   let result = [];
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === '--discard-next') {
-      result.push(arr[i + 1]);
+    if (arr[i] === '--discard-prev') {
+      result.pop();
     } 
-    else if (arr[i] === '--discard-prev') {
-      result.push(arr[i - 1]);
-    }
     else if (arr[i] === '--double-next') {
-      result.push(arr[i + 1], arr[i + 1]);
+      result.push(arr[i + 1]);
     }
     else if (arr[i] === '--double-prev') {
-      result.push(arr[i - 1], arr[i - 1]);
+      result.push(arr[i - 1]);
+    }
+    else if (arr[i] === '--discard-next') {
+      if (arr[i + 2] && arr[i + 2].toString().includes('-prev')) {
+        i += 2;
+      }
+      else {
+        i += 1;
+      }
     }
     else {
       result.push(arr[i]);
     }
   };
-  return result.filter(Boolean);
+  return result.filter(i => i !== undefined);
 };
