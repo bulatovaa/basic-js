419e92f3ce4618cb1b8d8d74feb9c509f41acf87
Ekaterina Bulatova <katerinavitalevna@ya.ru>
2020-10-26 12:04:34 +0400

    feat: solve simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,21 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  arr: [],
+
   getLength() {
-    return chainMaker.length;
+    return this.arr.length;
   },
+
   addLink(value) {
-    return (value === undefined) ? chainMaker += '(  )' : chainMaker += '( ' + String(value) + ' )';
+    this.arr.push(String(value));
+    return this;
   },
+
   removeLink(position) {
-    
+    if (typeof position !== 'number' || position < 0 || position >= this.arr.length) {
+      this.arr = [];
+      throw Error;
+    }
+    this.arr.splice(position - 1, 1);
+    return this;
   },
+
   reverseChain() {
-    
+    this.arr.reverse();
+    return this;
   },
+
   finishChain() {
-    return chainMaker;
-  }
+    const result = this.arr.join(' )~~( ');
+    this.arr = [];
+    return `( ${result} )`;
+  },
 };
 
 module.exports = chainMaker;
